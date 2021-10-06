3254f07b8771d5e5d90085273b53b0c4739b4db2
Aditya Verma <aditya.verma5225@gmail.com>
2021-10-06 17:03:39 +0530

    Initial commit

---
+++ app/src/main/java/com/android/wallum/Wallum.kt
@@ -0,0 +1,11 @@
+package com.android.wallum
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+Created By Aditya Verma on 06/10/21
+ **/
+
+@HiltAndroidApp
+class Wallum : Application()
